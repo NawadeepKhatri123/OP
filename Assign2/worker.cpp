f764451408e6082bc1f41ddf4aac522aaad8f595
NawadeepKhatri123 <5086950@nwpolytech.ca>
2024-10-16 11:58:54 -0600

    final

--- Assign2/worker.cpp
+++ Assign2/worker.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
-#include <cstdlib>
+#include <cstdlib> // to use atoi ( str to int)
 
-void print_primes(int start, int end);
-bool is_prime(int num);
+void print_primes(int start , int end);
+bool calc_prime (int num);
+
+
 
 
 int main(int argc , char *argv[]){
-    int start = atoi (argv[1]);
-    int end = atoi (argv[2]);
+    int start = atoi (argv[1]);           // convert the first num to int which is the start of the range
+    int end = atoi (argv[2])  ;          // convert the second num to int which is the end of the range
 
-    print_primes(start,end);
-    return 0;
+    print_primes(start , end); // call print_primes function 
+    return 0;                           // end the program
 }
 
 
-void print_primes(int start, int end){
-    std::cout << "the primes between " << start << " and " << end <<" is : ";
-
-    for (int i = start; i <= end ; i++ ){
-        if ( is_prime(i)){
-            std::cout << i << " ";
+void print_primes (int start, int end){    // takes two arguments
+    std ::cout << "primes from " << start << " to " << end <<" is : ";  // prints primes from ...........
+    for (int i = start ; i <= end ; i++ ){                                   // loop to check for prime
+        if ( calc_prime(i)){                                                 // send the num into te check prime function
+            std::cout << i << " " ;
         }
     } 
-    std::cout << '\n';
-}
+    std::cout<<'\n' ;                                                        // new line after printing all the primes of the range
+}   
 
-bool is_prime(int num){
-    if (num <= 1) return false;
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) return false;
-    }
-    return true;
-} 
+bool calc_prime (int num){                                                   // calculates prime
+   if ( num <= 1 )      return false;                                        // checks if <=1 then no prime
+   else if (num == 2 )    return true;                                       // if 2 then prime
+   else if (num % 2 == 0 ) return false;                                     // if %2 = 0 then not prime
+   for (int i = 3 ; (i*i) <= num; i += 2){                               // ???????
+        if ( num % i == 0) return false;
+   }
+   return true;                                                              //return true
+}
