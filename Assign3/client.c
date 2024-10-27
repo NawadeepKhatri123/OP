6e8122111bf0baba3115b43ebd2f20bfcf3b4873
NawadeepKhatri123 <5086950@nwpolytech.ca>
2024-10-27 16:30:26 -0600

    good

--- Assign3/client.c
+++ Assign3/client.c
@@ -1,3 +1,4 @@
+
 // C program to implement one side of FIFO
 // This side writes first, then reads
 #include <stdio.h>
@@ -9,48 +10,39 @@
 
 int main()
 {
-	int fd;//file descriptor
+	int fd;
 
 	// FIFO file path
-    const char * myfifo = "/tmp/myfifo";// create a temp file for a named pipe
+	const char * myfifo = "/tmp/myfifo";
 
 	// Creating the named file(FIFO)
 	// mkfifo(<pathname>, <permission>)
-	//mkfifo(myfifo, 0666);owner has read // write execution on the file and the world
-    //cllient doesnot have to make a pipe
+	mkfifo(myfifo, 0666);
+
 	char arr1[80], arr2[80];
-    while (1)
-	{
+
 		// Open FIFO for write only
-		fd = open(myfifo, O_WRONLY);	//O_WRONLY write only
+		fd = open(myfifo, O_WRONLY);
 
 		// Take an input arr2ing from user.
 		// 80 is maximum length
-		fgets(arr2, 80, stdin);		// gets a string from a streami into the array		GOTO LINUX MANUAL TO UNDERSTAND 
+		fgets(arr2, 80, stdin);
 
 		// Write the input arr2ing on FIFO
 		// and close it
-		write(fd, arr2, strlen(arr2)+1);		//Takes a file descriptor 
+		write(fd, arr2, strlen(arr2)+1);
 		close(fd);
-        
 
 		// Open FIFO for Read only
 		fd = open(myfifo, O_RDONLY);
 
 		// Read from FIFO
 		read(fd, arr1, sizeof(arr1));
-        ssize_t bytesRead = read(fd, arr1, sizeof(arr1));
-        if (bytesRead > 0) {
-            printf("Received: %s", arr1);
-        }
 
 		// Print the read message
-        printf("prime numbers are = ");
-		printf( arr1);
-      
+		printf("User2: %s\n", arr1);
 		close(fd);
-        
-	}
+
 	return 0;
 }
 
